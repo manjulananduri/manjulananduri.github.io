e7fedbced3f98fb488922c1d9775b718b8841460
Pramod Nanduri <pramodnanduri@ymail.com>
2021-04-25 16:32:33 -0700

    Interview tab addition

--- src/framework/FileGenerator.java
+++ src/framework/FileGenerator.java
@@ -8,9 +8,9 @@ import java.util.stream.Stream;
 
 public class FileGenerator {
     public static void main(String[] args) {
-        String title = "Word Search";
-        String postTitle = "Word Search (Leetcode 79) - Top Coding Interview Question";
-        String postFileSuffix = "-top-coding-interview-question";
+        String title = "Cherry Pickup II";
+        String postTitle = "Cherry Pickup II (Leetcode 1463)";
+        String postFileSuffix = "-amazing-coding-interview-question";
         new FileGenerator().generate(title, postTitle, postFileSuffix);
     }
 
